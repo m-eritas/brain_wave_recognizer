297120970679935923a856e4ac85f7b8b82c408e
m_eritas <92969191+m-eritas@users.noreply.github.com>
2026-06-03 15:07:07 +0200

    Update brainwave_recognizer.hpp

--- hls/src/brainwave_recognizer.hpp
+++ hls/src/brainwave_recognizer.hpp
@@ -3,11 +3,11 @@
 
 // ──────────────────────────────────────────────────────────────
 // Top-level HLS function you’re synthesising.
-// Keep the signature EXACTLY the same as in brainwave_core.cpp
+// Keep the signature EXACTLY the same as in FIR.cpp
 // ──────────────────────────────────────────────────────────────
 void brainwave_recognizer(
     ap_int<16>  sample,      // one input sample (Q2.14)
     ap_uint<3>  band_sel,    // 0-4  → which coefficient set
     ap_uint<2>  sens_sel,    // 0-2  → threshold level
-    bool       &flag_out     // goes high when burst detected
+    bool&       lag_out     // goes high when burst detected
 );
