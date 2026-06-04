344e42d970041aba4e46de0be56b23ecb1717d5d
m-eritas <merituks@gmail.com>
2026-06-04 15:48:20 +0200

    Added new tcl file for exporting IP

---
+++ hls/run_export_ip.tcl
@@ -0,0 +1,18 @@
+catch {file delete -force brain_wave_detector}
+
+open_project brain_wave_detector
+set_top brainwave_recognizer
+
+add_files src/brainwave_recognizer.hpp
+add_files src/brainwave_recognizer.cpp
+add_files -tb tb/tb.cpp
+
+open_solution solution1 -flow_target vivado
+set_part {xa7a12tcpg238-2I}
+create_clock -period 10.0 -name default
+
+csynth_design
+cosim_design -rtl vhdl
+export_design -rtl vhdl -format ip_catalog
+
+exit
