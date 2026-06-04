d5104869e824d61ba66fe456f3897beb52af8c8f
m_eritas <92969191+m-eritas@users.noreply.github.com>
2026-06-04 14:11:02 +0200

    0_M2 Sanity Check
    Updated function signature and comments in brainwave_recognizer.

--- hls/src/brainwave_recognizer.hpp
+++ hls/src/brainwave_recognizer.hpp
@@ -3,11 +3,11 @@
 
 // ──────────────────────────────────────────────────────────────
 // Top-level HLS function you’re synthesising.
-// Keep the signature EXACTLY the same as in FIR.cpp
+// Keep the signature EXACTLY the same as in brain_wave_recognizer.cpp
 // ──────────────────────────────────────────────────────────────
 void brainwave_recognizer(
     ap_int<16>  sample,      // one input sample (Q2.14)
     ap_uint<3>  band_sel,    // 0-4  → which coefficient set
     ap_uint<2>  sens_sel,    // 0-2  → threshold level
-    bool&       lag_out     // goes high when burst detected
+    bool&       flag_out     // goes high when burst detected
 );
